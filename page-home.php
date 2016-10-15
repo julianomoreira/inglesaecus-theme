31a917291281a619907e791588287d463becd622
Juliano M Dasilva <julianomoreira@julianomoreira.com>
2016-10-15 13:36:10 -0500

    added images and edited text

--- page-home.php
+++ page-home.php
@@ -15,18 +15,18 @@
     <link href="<?php bloginfo('stylesheet_directory'); ?>/dist/css/style.css" rel="stylesheet">
   </head>
   <body>
-    <div class="row">
+    <div class="fullWidth">
       <div class="large-12 columns main-banner-bg">
         <div class="row">
           <div class="large-10 large-offset-2 large-centered .small-offset-3 columns main-banner">
             <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/logo-aecus.png" alt="Inglês Aecus" class="logo" />
             <h2 class="main-message">A escola pra quem quer estar na frente.</h2>
-            <p class="main-message-desc">Você pensou no que pode acontecer em um ano? Em simplesmente um ano, você alcaçará objetivos jamais imaginados. Aprender inglês na Escola Aecus irá transformar sua vida. Acredite em você!  Junta-se a centenas de pessoas que foram transformadas atravês da escola Inglês Aecus.</p>
+            <p class="main-message-desc">Você pensou no que pode acontecer em um ano? Em simplesmente um ano, você pode alcançar objetivos jamais imaginados com inglês. Aprender inglês na escola Inglês Aecus irá transformar sua vida. Acredite em você!  Junta-se a centenas de pessoas que foram transformadas atravês da escola Inglês Aecus.</p>
             <div class="row align-middle">
               <div class="large-12 large-centered main-banner__form ">
                 <div class="row">
                   <div class="large-5 medium-5 columns nopadding">
-                    <p class="form__desc">Cadastre no nosso site para receber conteúdo diretamente no seu inbox.</p>
+                    <p class="form__desc">Cadastre-se e receba dicas, materiais gratuitos, bônus e promoções diretamente no seu e-mail.</p>
                   </div>
                   <div class="large-2 medium-2 columns nopadding">
                     <input type="text" placeholder="Nome" />
@@ -48,32 +48,39 @@
       <div class="large-10 large-offset-2 large-centered columns main-content-wrapper">
         <div class="row">
           <div class="large-8 columns left-col">
-            <h3 class="section-title">Venha para melhor escola de inglês do nordeste!</h3>
-            <p>Em breve estaremos inaugurando o site da escola aecus. Uma escola que acredita que o dominio de idiomas significa crescimento, desenvolvimento e, acima de tudo, melhores condições de acompanhar as rápidas mudanças que veem ocorrendo nesse novo e tecnológico século.</p>
-            <h4 class="subsection-title">Criança até 12 anos</h4>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
+            <h3 class="section-title">Conheça a Escola de Inglês que se preocupa com seu aprendizado!</h3>
+            <p>A Inglês Aecus faz 20 anos atuando no mercado, promovendo um aprendizado rápido e eficaz.</p>
+            <h4 class="subsection-title">Metodologia</h4>
+            <p>Nossa metodologia é bastante versátil, enfatizando simultaneamente as quatro habilidades: ouvir, falar, ler e escrever.</p>
+            <h4 class="subsection-title">Material</h4>
+            <p>Os nossos livros de jovens e adultos são atualizados e contextualizados, com textos e diálogos baseados em situações do dia a dia e ainda conta com uma plataforma online 24 horas por dia, a disposição do aluno. Nele, o aluno pode praticar vocabulário, gramática, ver e ouvir os vídeos com situações bastante reais.</p>
+            <p>Assim como o material para adultos. O material para adolescentes é semelhante ao de adultos, com uma abordagem mais apropriada para a idade.</p>
+            <p>O material de crianças, é o que há de mais moderno hoje no mercado em termo de tecnologia e recursos, onde a criança mergulha no mundo da língua e tem acesso ao idioma de várias formas, como por exemplo, músicas, histórias, vídeos e acesso ao website 24 horas a sua disposição, nele o aluno encontra jogos e áudios tanto do livro quanto do caderno de exercício. </p>
             <hr />
-            <h4 class="subsection-title">Adultos</h4>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
+            <h4 class="subsection-title">Curso para crianças a partir de 4 anos de idade.</h4>
+            <p>Começando aos 4 anos de idade a criança aprenderá a língua de forma natural e desenvolverá inicialmente a compreensão auditiva e oralidade com mais facilidades que os jovens e adultos.</p>
+            <p>Está comprovado que  a melhor idade para se aprender línguas, do ponto de vista cognitivo, é de zero aos nove anos. Nesse período, a criança não terá desenvolvido totalmente o seu autocrítica, portanto ela não terá a preocupação de errar, pois ela utiliza o que está aprendendo de forma espontánea. </p>
+            <p>Aplicamos uma metodologia dinâmica e lúdica, tornando o aprendizado uma divertida atividade. Nosso papel é proporcionar um ambiente onde as crianças possas sentir motivadas a aprender de forma eficaz. </p>
             <hr />
-            <h4 class="subsection-title">aecusTOEFL</h4>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
+            <h4 class="subsection-title">Adolescentes</h4>
+            <p>Trazemos para dentro de sala de aula as mais diversas situações do cotidiano americano, com uma linguagem jovem e dinâmica, preparando o aluno para a vida profissional e acadêmica.</p>
+            <h4 class="subsection-title">Curso para jovens e adultos</h4>
+            <p>Preparamos nossos alunos para as mais diversas situações do cotidiano da língua inglesa, inclusive para testes de proficiência, proporcionando a autoconfiança para se expressarem.</p>
             <hr />
-            <h4 class="subsection-title">aecusVIP</h4>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
-            <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
+            <h4 class="subsection-title">Avaliação</h4>
+            <p>O nosso processo avaliativo envolve desempenho escrito, oral e compreensão auditiva. O aluno está apto a mudar de nível e receber certificado, quando alcança a média 7 (sete) em cada avaliação.</p>
+            <h4 class="subsection-title">Certificação Internacional</h4>
+            <p>Além dos certificados de aproveitamento de cada estágio e de conclusão do curso, o nosso aluno pode fazer o TOEFL JUNIOR, válido em todo território nacional e o TOEIC,  reconhecido INTERNACIONALMENTE, muito solicitado por grandes empresas.</p>
           </div>
           <div class="large-4 columns right-col">
             <div class="">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/photo-julio.png" alt="Foto de Julio Bernardino, diretor aecus" class="photo-julio" />
-              <h4 class="subsection-title">Julio Bernardino, diretor</h4>
-              <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/photo-julio.jpg" alt="Foto de Julio Bernardino, diretor aecus" class="photo-julio" />
+              <h4 class="subsection-title">Julio Bernardino, diretor e professor</h4>
+              <p>Eu tenho me dedicado ao ensino de inglês na região do Vale do São Francisco por mais de 20 anos. Meu objetivo é ajudar pessoas a transformar sua vida profissional através do inglês.</p>
+              <p>Entre em contato conosco abaixo caso tenha alguma dúvida sobre a nossa metodologia ou sobre a escola.</p>
               <hr />
               <h4 class="subsection-title">Entre em contato conosco</h4>
-              <p>Lorem ipsum Curabitur diam a felis facilisis. Diam ornare. Velit in. Vitae Morbi volutpat mi In vestibulum lorem, a felis. Vestibulum Proin eu. Accumsan at tincidunt metus vel pellentesque. Amet.</p>
+              <p>Precisando de aprender inglês e não sabe por onde começar? Dúvidas sobre o método ou carga horária? Entre em contato conosco. Estamos a sua disposição!</p>
               <h5 class="sm-subsection-title icon-facebook">Facebook</h5>
               <p><a href="https://facebook.com/inglesaecus">facebook.com/inglesaecus</a></p>
               <h5 class="sm-subsection-title icon-email">E-mail</h5>
---
+++ page-sales.php
@@ -0,0 +1,98 @@
+<?php
+/*
+  Template Name: Sales Page
+*/
+
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inglês Aecus</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,400i,600,600i" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/dist/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="fullWidth">
+      <div class="large-12 columns main-banner-bg">
+        <div class="row">
+          <div class="large-10 large-offset-2 large-centered .small-offset-3 columns main-banner">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/logo-aecus.png" alt="Inglês Aecus" class="logo" />
+            <h2 class="main-message">Quer aprender inglês em pouco tempo?</h2>
+            <p class="main-message-desc">Você pensou no que pode acontecer em um ano? Em simplesmente um ano, você alcaçará objetivos jamais imaginados. Aprender inglês na Escola Aecus irá transformar sua vida. Acredite em você!  Junta-se a centenas de pessoas que foram transformadas atravês da escola Inglês Aecus.</p>
+            <div class="row align-middle">
+              <div class="large-12 large-centered ">
+                <div class="row">
+                  <a href="https://truffilio.leadpages.co/leadbox/140c18f73f72a2%3A140d6ded6746dc/5667908084563968/" target="_blank">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/cta-curso-intensivo.png" alt="Cadastre e recebe um bonus" class="text-center cta-curso-intensivo" />
+                  </a>
+                  <script data-leadbox="140c18f73f72a2:140d6ded6746dc" data-url="https://truffilio.leadpages.co/leadbox/140c18f73f72a2%3A140d6ded6746dc/5667908084563968/" data-config="%7B%7D" type="text/javascript" src="https://truffilio.leadpages.co/leadbox-1476481805.js"></script>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="large-10 large-offset-2 large-centered columns main-content-wrapper">
+        <div class="row">
+          <div class="large-8 columns left-col">
+            <h3 class="section-title">Matricule-se no curso intensivo de 3 meses!</h3>
+            <p>A Inglês Aecus está oferecendo um programa intensivo de fim de ano com duração de 3 meses. A carga horária por módulo é de 80h, sendo 6h semanais de outubro até dezembro e em janeiro 15h semanais 3h por dia.</p>
+            <p>Durantes as aulas, focamos nas  quatro habilidades da Língua que são: ouvir, falar, ler e escrever. Contamos com um material que apresenta situações voltadas para o dia-a-dia americano, com uma plataforma Online disponível 24h por dia onde o aluno pode praticar o vocabulário, a gramática, ver e ouvir os vídeos de forma interativa e dinâmica.</p>
+
+            <p><strong>Início:</strong> 24 de outubro de 2016  </p>
+            <p><strong>Término:</strong> 31 de janeiro de 2017</p>
+
+            <h4 class="subsection-title">Dias e horários das aulas de outubro a dezembro:</h4>
+            <p><strong>Terça, Quintas e Sextas</strong></p>
+
+            <p><strong>Manhã:</strong> das 09:00 às 11:00</p>
+            <p><strong>Tarde:</strong> das 15:00 às 17:00</p>
+            <p><strong>Noite:</strong> das 18:30 às 20:30</p>
+
+            <h4 class="subsection-title">Dias e horários das aulas em Janeiro:</h4>
+            <p><strong>Segunda a Sexta:</strong></p>
+
+            <p><strong>Manhã:</strong> das 09:00 às 12:00</p>
+            <p><strong>Tarde:</strong> das 14:30 às 17:30</p>
+            <p><strong>Noite:</strong> das 18:30 às 21:30</p>
+          </div>
+          <div class="large-4 columns right-col">
+            <div class="">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/photo-julio.jpg" alt="Foto de Julio Bernardino, diretor aecus" class="photo-julio" />
+              <h4 class="subsection-title">Julio Bernardino, diretor e professor</h4>
+              <p>Eu tenho me dedicado ao ensino de inglês na região do Vale do São Francisco por mais de 20 anos. Meu objetivo é ajudar pessoas a transformar sua vida profissional através do inglês.</p>
+              <p>Entre em contato comigo caso tenha alguma dúvida sobre a nossa metodologia ou sobre a escola.</p>
+              <hr />
+              <h4 class="subsection-title">Entre em contato conosco</h4>
+              <p>Precisando de aprender inglês e não sabe por onde começar? Dúvidas sobre o método ou carga horária? Entre em contato conosco. Estamos a sua disposição!</p>
+              <h5 class="sm-subsection-title icon-facebook">Facebook</h5>
+              <p><a href="https://facebook.com/inglesaecus">facebook.com/inglesaecus</a></p>
+              <h5 class="sm-subsection-title icon-email">E-mail</h5>
+              <p><a href="https://facebook.com/inglesaecus">secretaria@inglesaecus.com.br</a></p>
+              <h5 class="sm-subsection-title icon-fone">Fone</h5>
+              <p><a href="https://facebook.com/inglesaecus">87.3861.5510</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="large-10 large-offset-2 large-centered columns">
+        <div class="footer">
+          <p><strong>Estamos localizado na Av. Josimar Batista da Silva, Numero 140 - Bairro Maria Auxiliadora <br />Petrolina - PE CEP: 56.304.620</strong></p>
+          <p><strong>Copyright Inglês Aecus 2016</strong></p>
+        </div>
+      </div>
+    </div>
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/what-input/what-input.js"></script>
+    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
